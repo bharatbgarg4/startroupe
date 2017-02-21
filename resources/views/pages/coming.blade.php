670c85a77c12a580f39cf203339c569ba57194dc
Bharat Garg <bharatbgarg4@gmail.com>
2017-02-21 12:42:07 +0530

    meta

--- resources/views/pages/coming.blade.php
+++ resources/views/pages/coming.blade.php
@@ -3,11 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="keywords" content="" />
-	<meta name="description" content=""/>
+	<meta name="description" content="Startroupe.com is a complete marketplace for finding and hiring best actors, dancers, photographers, singers, rj, vj, makeup artists, performers, comedians and much more."/>
 	<link href="/logo.png" rel="icon" type="image/x-icon" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Startroupe</title>
+	<title>Talent Marketplace | Actors, singers, models, dancers, photographers.</title>
 	<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
 	@yield('headinclude')
